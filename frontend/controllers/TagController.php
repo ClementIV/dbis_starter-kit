064455cdade77f15dc42b3792854b58e9474e798
angela2015 <1668289330@qq.com>
2017-05-18 17:19:29 +0800

    标签

---
+++ frontend/controllers/TagController.php
@@ -0,0 +1,132 @@
+<?php
+
+namespace frontend\controllers;
+
+use Yii;
+use common\models\Tag;
+use frontend\models\search\TagSearch;
+use yii\web\Controller;
+use yii\web\NotFoundHttpException;
+use yii\filters\VerbFilter;
+use frontend\models\search\UserTeacherSearch;
+use common\models\Project;
+use frontend\models\search\ProjectSearch;
+
+/**
+ * TagController implements the CRUD actions for Tag model.
+ */
+class TagController extends Controller
+{
+    /**
+     * @inheritdoc
+     */
+    public function behaviors()
+    {
+        return [
+            'verbs' => [
+                'class' => VerbFilter::className(),
+                'actions' => [
+                    'delete' => ['POST'],
+                ],
+            ],
+        ];
+    }
+
+    /**
+     * Lists all Tag models.
+     * @return mixed
+     */
+    public function actionIndex()
+    {
+        $searchModel = new TagSearch();
+        $dataProvider = $searchModel->searchOneTag(Yii::$app->request->queryParams);
+        $model = $dataProvider->getModels()[0];
+        $searchModel = new UserTeacherSearch();
+        $dataProviderTeacher = $searchModel->searchTeacherwithTag(Yii::$app->request->queryParams['tagid']);
+        $searchModel1 = new ProjectSearch();
+        $dataProviderProject = $searchModel1->searchwithTag(Yii::$app->request->queryParams['tagid']);
+        return $this->render('index', [
+            'searchModel' => $searchModel,
+            'dataProvider' => ['0'=>$dataProviderTeacher,'1'=> $dataProviderProject],
+            'model' => $model
+        ]);
+    }
+
+    /**
+     * Displays a single Tag model.
+     * @param integer $id
+     * @return mixed
+     */
+    public function actionView($id)
+    {
+        return $this->render('view', [
+            'model' => $this->findModel($id),
+        ]);
+    }
+
+    /**
+     * Creates a new Tag model.
+     * If creation is successful, the browser will be redirected to the 'view' page.
+     * @return mixed
+     */
+    public function actionCreate()
+    {
+        $model = new Tag();
+
+        if ($model->load(Yii::$app->request->post()) && $model->save()) {
+            return $this->redirect(['view', 'id' => $model->id]);
+        } else {
+            return $this->render('create', [
+                'model' => $model,
+            ]);
+        }
+    }
+
+    /**
+     * Updates an existing Tag model.
+     * If update is successful, the browser will be redirected to the 'view' page.
+     * @param integer $id
+     * @return mixed
+     */
+    public function actionUpdate($id)
+    {
+        $model = $this->findModel($id);
+
+        if ($model->load(Yii::$app->request->post()) && $model->save()) {
+            return $this->redirect(['view', 'id' => $model->id]);
+        } else {
+            return $this->render('update', [
+                'model' => $model,
+            ]);
+        }
+    }
+
+    /**
+     * Deletes an existing Tag model.
+     * If deletion is successful, the browser will be redirected to the 'index' page.
+     * @param integer $id
+     * @return mixed
+     */
+    public function actionDelete($id)
+    {
+        $this->findModel($id)->delete();
+
+        return $this->redirect(['index']);
+    }
+
+    /**
+     * Finds the Tag model based on its primary key value.
+     * If the model is not found, a 404 HTTP exception will be thrown.
+     * @param integer $id
+     * @return Tag the loaded model
+     * @throws NotFoundHttpException if the model cannot be found
+     */
+    protected function findModel($id)
+    {
+        if (($model = Tag::findOne($id)) !== null) {
+            return $model;
+        } else {
+            throw new NotFoundHttpException('The requested page does not exist.');
+        }
+    }
+}
