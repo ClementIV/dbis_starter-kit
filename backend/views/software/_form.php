36d2e3afda4eae11d361709d275c37c3d983a831
angela2015 <1668289330@qq.com>
2017-06-22 16:02:24 +0800

    软件著作权后台上传附件

--- backend/views/software/_form.php
+++ backend/views/software/_form.php
@@ -2,7 +2,7 @@
 
 use yii\helpers\Html;
 use yii\bootstrap\ActiveForm;
-
+use trntv\filekit\widget\Upload;
 /* @var $this yii\web\View */
 /* @var $model common\models\Software */
 /* @var $form yii\bootstrap\ActiveForm */
@@ -25,6 +25,16 @@ use yii\bootstrap\ActiveForm;
     <?php echo $form->field($model, 'regisnumber')->textInput(['maxlength' => true]) ?>
 
     <?php echo $form->field($model, 'enclosure')->textInput(['maxlength' => true]) ?>
+
+    <?php echo $form->field($model, 'attachments')->widget(
+        Upload::className(),
+        [
+            'url' => ['/file-storage/upload'],
+            'sortable' => true,
+            'maxFileSize' => 10000000, // 10 MiB
+            'maxNumberOfFiles' => 10
+        ]);
+    ?>
 
     <div class="form-group">
         <?php echo Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
--- backend/views/software/index.php
+++ backend/views/software/index.php
@@ -4,7 +4,7 @@ use yii\helpers\Html;
 use yii\grid\GridView;
 
 /* @var $this yii\web\View */
-/* @var $searchModel common\models\query\SoftWareQuery */
+/* @var $searchModel common\models\query\SoftwareQuery */
 /* @var $dataProvider yii\data\ActiveDataProvider */
 
 $this->title = 'Softwares';
